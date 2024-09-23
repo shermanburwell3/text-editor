b603f98ca345b880fd37eefc55928ac0aa0e56de
Sherman Burwell <shermanburwell3@gmail.com>
2024-09-23 12:56:46 -0500

    Implemented asset caching, modified path for index.html in webpack config

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,19 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     plugins: [
       // Generates an HTML file from a template
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: './index.html',
         filename: 'index.html',
       }),
       // Generates a manifest.json file for PWA
