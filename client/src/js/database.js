a9e3a17961b2b941a3dc7eef16ec058811899f25
Sherman Burwell <shermanburwell3@gmail.com>
2024-09-23 13:17:02 -0500

    Added indexedDb code to client/src/js/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const request = store.add({ content }); // Add the content to the object store
+  const result = await request;
+
+  console.log('Data saved to the database:', result);
+};
+
+// Gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll(); // Get all content from the object store
+  const result = await request;
+
+  console.log('Data retrieved from the database:', result);
+  return result; // Return the retrieved data
+};
 
 initdb();
